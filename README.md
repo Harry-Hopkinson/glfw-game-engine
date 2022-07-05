2e3dd4a8af4c454f66781562f30324a571c88f15
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2022-07-05 19:51:35 +0000

    Make File

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A Game Engine created in C
 ## Building
 
 ```bash
+cd scripts
 chmod +x build.bat
 build.bat
 ```
--- scripts/build.bat
+++ scripts/build.bat
@@ -1,1 +1,4 @@
-echo "Hello World"
+set files= src/main.c
+set libs=W:\lib\SDL2main.lib W:\lib\SDL2.lib W:\lib\freetype.lib
+
+g++ ../src/main.c -o bin\main.exe ../lib
